64451da550aecec433f0f461f10b7c9096d18027
Muhammad Nouman <ahmedsadiq.git@gmail.com>
2022-03-17 23:39:34 +0500

    Latest Code

---
+++ AuthenticateYourselfUITests/AuthenticateYourselfUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AuthenticateYourselfUITestsLaunchTests.swift
+//  AuthenticateYourselfUITests
+//
+//  Created by Ahmed Sadiq on 17/03/2022.
+//
+
+import XCTest
+
+class AuthenticateYourselfUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AuthenticateYourself' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Alamofire', '~> 4.3'
+  pod 'SwiftyJSON', '3.0.0'
+  pod 'Toast-Swift'
+  pod 'PhoneNumberKit', '~> 3.2.0'
+  pod 'IQKeyboardManagerSwift'
+  pod 'SKCountryPicker'
+  # Pods for AuthenticateYourself
+
+  target 'AuthenticateYourselfTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AuthenticateYourselfUITests' do
+    # Pods for testing
+  end
+
+end
